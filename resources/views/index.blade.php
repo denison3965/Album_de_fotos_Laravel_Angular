88bb66de2e4e78d2f6682f4bca3fabd7a1f0ae9b
Denison <denisondealmeidaprtela@hotmail.com>
2021-07-11 18:06:42 -0300

    Integração com laravel e Angular feito e primeira parte do front end feito

---
+++ resources/views/index.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+    </head>
+    <body>
+
+        <app-root></app-root>
+        <script src="{{asset('js/runtime.bc4ce951e505b9e7d62d.js')}}" defer></script>
+        <script src="{{asset('js/polyfills.f161bf4a91cb3d66b739.js')}}" defer></script>
+        <script src="{{asset('js/main.6eada6864f4048331ece.js')}}" defer></script>
+
+    </body>
+</html>
